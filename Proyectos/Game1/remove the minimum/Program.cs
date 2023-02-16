7097d9dfd826635f2673ce902eb23a4cd4918bd9
Peng <penghe201211@gmail.com>
2023-02-16 12:24:27 -0300

    Course page updated

--- Proyectos/Game1/remove the minimum/Program.cs
+++ Proyectos/Game1/remove the minimum/Program.cs
@@ -1,20 +1,29 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MyNamespace
 {
-    static void Main(string[] args)
-    { var myList = new List<int> () { 1,3,6,8,1,3};
-        Remover.RemoveSmallest(myList);
-        foreach (var item in myList)
+    public class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine(item);
+            var myList = new List<int>() { 1, 2, 3, 4, 5 };
+            Remover.RemoveSmallest(myList);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
-}
-public class Remover
-{
-    public static List<int> RemoveSmallest(List<int> numbers)
+    public class Remover
     {
-       
-          numbers.Distinct().ToList().Remove(numbers.Min());
-        return numbers;
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            var newNumbers = numbers;
+            newNumbers.Remove(newNumbers.Min());
+
+            return newNumbers;
+        }
     }
+
 }
